a24b3c2f041b5dda4aea84ca5fe5ee130aa64599
Crackexy <crackexy@gmail.com>
2020-10-18 11:26:54 +0530

    Update alternate.py

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -1,4 +1,12 @@
+import sys
+import traceback
+
 from functools import wraps
+from typing import Optional
+
+from telegram import User, Chat, ChatMember, Update, Bot
+
+from tg_bot import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
 from telegram import error, ChatAction
 
 
@@ -8,32 +16,3 @@ def send_message(message, text, *args, **kwargs):
     except error.BadRequest as err:
         if str(err) == "Reply message not found":
             return message.reply_text(text, quote=False, *args, **kwargs)
-
-
-def typing_action(func):
-    """Sends typing action while processing func command."""
-
-    @wraps(func)
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(
-            chat_id=update.effective_chat.id, action=ChatAction.TYPING
-        )
-        return func(update, context, *args, **kwargs)
-
-    return command_func
-
-
-def send_action(action):
-    """Sends `action` while processing func command."""
-
-    def decorator(func):
-        @wraps(func)
-        def command_func(update, context, *args, **kwargs):
-            context.bot.send_chat_action(
-                chat_id=update.effective_chat.id, action=action
-            )
-            return func(update, context, *args, **kwargs)
-
-        return command_func
-
-    return decorator
