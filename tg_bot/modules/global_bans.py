cc6f7175c15a4ab6a7cb5d80d41451828433a75f
Crackexy <crackexy@gmail.com>
2020-10-19 21:24:12 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -1,23 +1,18 @@
 import html
 from io import BytesIO
+from typing import Optional, List
 
-from telegram import ParseMode, Bot
+from telegram import Message, Update, Bot, User, Chat, ParseMode
 from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
 import tg_bot.modules.sql.global_bans_sql as sql
-from tg_bot import (
-    dispatcher,
-    OWNER_ID,
-    SUDO_USERS,
-    SUPPORT_USERS,
-    STRICT_GBAN,
-    MESSAGE_DUMP,
-)
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, STRICT_GBAN, MESSAGE_DUMP
 from tg_bot.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot.modules.helper_funcs.misc import send_to_list
 from tg_bot.modules.sql.users_sql import get_all_chats
 
 GBAN_ENFORCE_GROUP = 6
