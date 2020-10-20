b6b07d1bffcc693a98ba5c8549bddaa155e5f78e
Crackexy <crackexy@gmail.com>
2020-10-20 12:06:31 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -302,7 +302,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 /gbanstat
 """
 
-__mod_name__ = "Global Bans"
+__mod_name__ = no name
 
 GBAN_HANDLER = CommandHandler("gban", gban, pass_args=True,
                               filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
